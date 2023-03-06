be901864e34ab38072974d388cc8a5a67e3f1a75
Hippolyte <deparis.hipp@gmail.com>
2023-03-06 11:08:50 +0100

    debut

--- main.cpp
+++ main.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+class Objet {
+    private:
+        int id;
+        int consoVeh;
+        int benef;
+};
+
+class Vehicule {
+    private :
+        int capacity;
+        int nbObj;
+        std::vector<Objet> objets;
+};
+
+class Villes {
+    private:
+        int nbVilles;
+        std::vector<std::string> nomVilles;
+
+};
+
+void lectureFichiers(std::string name) {
+    std::ifstream fichier(name, std::ios::in);
+
+    if(fichier) {
+
+    } else {
+        std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
+    }
+}
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
