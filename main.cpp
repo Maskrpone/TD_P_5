f12fe274da516168bb90301a1e48724e8cb25d1b
Hippolyte <deparis.hipp@gmail.com>
2023-03-06 11:14:14 +0100

    lecture fichier

--- main.cpp
+++ main.cpp
@@ -3,37 +3,51 @@
 #include <fstream>
 #include <string>
 
-class Objet {
-    private:
-        int id;
-        int consoVeh;
-        int benef;
+class Objet
+{
+private:
+    int id;
+    int consoVeh;
+    int benef;
 };
 
-class Vehicule {
-    private :
-        int capacity;
-        int nbObj;
-        std::vector<Objet> objets;
+class Vehicule
+{
+private:
+    int capacity;
+    int nbObj;
+    std::vector<Objet> objets;
 };
 
-class Villes {
-    private:
-        int nbVilles;
-        std::vector<std::string> nomVilles;
-
+class Villes
+{
+private:
+    int nbVilles;
+    std::vector<std::string> nomVilles;
 };
 
-void lectureFichiers(std::string name) {
+void lectureFichiers(std::string name)
+{
     std::ifstream fichier(name, std::ios::in);
 
-    if(fichier) {
-
-    } else {
-        std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
+    if (fichier)
+    {
+        std::string ligne;
+        while(std::getline(fichier, ligne))
+        {
+                std::cout << ligne << std::endl;
+        }
+    }
+    else
+    {
+        std::cerr << "Impossible d'ouvrir le fichier !";
+        return;
     }
 }
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int, char **)
+{
+    std::string name = "../4villes.txt";
+    lectureFichiers(name);
+    return 0;
 }
